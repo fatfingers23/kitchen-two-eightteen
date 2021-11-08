b8a5b27eee76606b7ed1fa36fc81437a4e7bc2d2
Bailey Townsend <baileytownsend2323@gmail.com>
2021-11-08 17:30:46 -0600

    Okay last try

--- docs/_nuxt/static/1636413762/menu/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fkitchen-two-eightteen\u002F_nuxt\u002Fstatic\u002F1636413762",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmenu",config:{_app:{basePath:"\u002Fkitchen-two-eightteen\u002F",assetsPath:"\u002Fkitchen-two-eightteen\u002F_nuxt\u002F",cdnURL:a}}}}(null));
